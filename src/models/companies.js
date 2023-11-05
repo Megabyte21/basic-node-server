4fbd30fe076463bafc67ce694682445e74500c1d
Megabyte21 <roelrivera0176@gmail.com>
2023-11-05 14:15:16 -0600

    added department creations

--- src/models/companies.js
+++ src/models/companies.js
@@ -2,6 +2,10 @@ import db from '../utils/db'
 
 export const getCompanies = async () => db.company.findMany()
 
-export const getCompany = async (id) => db.company.findUnique({ where: { id } })
+export const getCompany = async (id) =>
+  db.company.findUnique({
+    where: { companyId: id },
+    include: { Department: true },
+  })
 
 export const addCompany = async (name) => db.company.create({ data: { name } })
